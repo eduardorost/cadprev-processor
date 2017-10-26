332c63eca3039c03b074cce46f4bbb124a0f7a30
ilegra.eduardo <eduardo.rost@ilegra.com>
2017-10-26 17:35:31 -0200

    initial commit

---
+++ cadprev/src/main/java/com/cadprev/CadprevApplication.java
@@ -0,0 +1,18 @@
+package com.cadprev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadprevApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadprevApplication.class, args);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+	}
+
+}
