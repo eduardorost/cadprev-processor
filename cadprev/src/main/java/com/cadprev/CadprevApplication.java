6388929480aacca40cee7369494a10d7b8e9a358
eduardorost <eduardo.a.rost@gmail.com>
2017-10-26 23:42:04 -0200

    example openning google on firefox

--- cadprev/src/main/java/com/cadprev/CadprevApplication.java
+++ cadprev/src/main/java/com/cadprev/CadprevApplication.java
@@ -1,7 +1,17 @@
 package com.cadprev;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CadprevApplication implements ApplicationRunner {
@@ -12,7 +22,23 @@ public class CadprevApplication implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
+		WebDriver driver = new FirefoxDriver();
+		driver.get("http://www.google.com");
+		WebElement element = driver.findElement(By.name("q"));
+		element.sendKeys("Cheese!\n"); // send also a "\n"
+		element.submit();
 
+		// wait until the google page shows the result
+		WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+				.until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
+
+		List<WebElement> findElements = driver.findElements(By.xpath("//*[@id='rso']//h3/a"));
+
+		// this are all the links you like to visit
+		for (WebElement webElement : findElements)
+		{
+			System.out.println(webElement.getAttribute("href"));
+		}
 	}
 
 }
